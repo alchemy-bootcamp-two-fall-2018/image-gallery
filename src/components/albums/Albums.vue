6e5b6c6358625efb3adce5fd5064e3bd17b1bdc3
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-29 15:01:00 -0800

    we made the albumsApi and the Albums components, switching drivers

---
+++ src/components/albums/Albums.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2>This is Christmas Albums</h2>
+    <AlbumList v-bind:albums="albums"/>
+  </section>
+</template>
+
+<script>
+import albumsApi from '../../albumsApi.js';
+import AlbumList from './AlbumList.vue';
+
+export default {
+  data() {
+    return {
+      albums: albumsApi.getAlbums()
+    };
+  },
+  components: {
+    AlbumList
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home';
+import Albums from './components/albums/Albums.vue';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
+    { path: '/albums', component: Albums },
+
   ]
 });
