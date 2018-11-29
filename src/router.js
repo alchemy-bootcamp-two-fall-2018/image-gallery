4e416ce55ce2a396d83c49063511ee508f7a4e68
Ivan <i.poblete915@gmail.com>
2018-11-29 15:09:56 -0800

    setup Albums.vue

--- src/router.js
+++ src/router.js
@@ -1,14 +1,10 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
-
+import Albums from './components/Albums/Albums.vue';
 
 export default new VueRouter({
   routes: [
-    { path: '/', component: Home }
-
+    { path: '/', component: Home },
+    { path: '/albums', component: Albums }
   ]
-
-
-
-
 });
