a20b01e57595548a799031db9f714de93dc834bc
JeiWolfei <jvampy22@gmail.com>
2018-11-29 15:44:12 -0800

    fixed naming issues and path errors

---
+++ src/components/albums/AlbumList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <Album 
+      v-for="album in albums"
+      :key="album.id"
+      :album="album"/>
+  </ul>
+</template>
+
+<script>
+import Album from './Albums';
+export default {
+  props: {
+    albums: Array
+  },
+  components: {
+    Album
+  }
+};
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0; left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, .5);
+}
+.content {
+  background: white;
+  padding: 40px;
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home';
 import Albums from './components/albums/Albums.vue';
+import AlbumDetail from './components/albums/AlbumDetail.vue';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/albums', component: Albums },
+    { path: '/albums/:id', component: AlbumDetail },
+
 
   ]
 });
