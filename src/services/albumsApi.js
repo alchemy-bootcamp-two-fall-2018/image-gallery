620f1be72d81b5138bc8afb5127307edf2fcb597
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-11-29 19:03:56 -0700

    added final images

--- src/services/albumsApi.js
+++ src/services/albumsApi.js
@@ -2,8 +2,18 @@ const albums = [
   { 
     id: 'AstroWorld',
     title: 'AstroWorld',
-    artist: 'Travis Scott'
-    // images: []
+    artist: 'Travis Scott',
+    images: [
+      {
+        title: 'AstroWorld cover',
+        url:'https://i.redd.it/9k136pcdiq601.jpg'
+      },
+      { 
+        title: 'Travis Scott',
+        url:'https://djbooth.net/.image/t_share/MTU4MTcwNTc0MzE1NTk1MDQ5/travis-scott-astroworld-nostalgia.jpg' 
+      }
+    ]
+    
 
   },
   {
