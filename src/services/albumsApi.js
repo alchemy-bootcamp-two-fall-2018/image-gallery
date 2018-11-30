d0191507d584a6ab86315770405f2e160711591c
Cheri Portman <cheriportman@hotmail.com>
2018-11-29 16:07:30 -0800

    data for jazz and blues

--- src/services/albumsApi.js
+++ src/services/albumsApi.js
@@ -1,15 +1,65 @@
 
 const albums = [
     { 
-        id: 'artist-name', 
-        title: 'Funk', 
+        id: 'bb-king', 
+        title: 'Blues', 
         images: [
             { 
-                title: 'Artist Name', 
-                url: 'https://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-102__605.jpg' 
+                title: 'BB King', 
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/B.B._King_in_2009.jpg/450px-B.B._King_in_2009.jpg' 
             }
         ] 
     }, 
+    {
+        id: 'bessie-smith', 
+        title: 'Blues', 
+        images: [
+            { 
+                title: 'Bessie Smith', 
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Bessie_Smith_%281936%29_by_Carl_Van_Vechten.jpg/330px-Bessie_Smith_%281936%29_by_Carl_Van_Vechten.jpg' 
+            }
+        ] 
+    },
+    {
+        id: 'robert-johnson', 
+        title: 'Blues', 
+        images: [
+            { 
+                title: 'Robert Johnson', 
+                url: 'https://upload.wikimedia.org/wikipedia/en/b/b3/Robert_Johnson.png' 
+            }
+        ] 
+    },
+    {
+        id: 'miles-davis', 
+        title: 'Jazz', 
+        images: [
+            { 
+                title: 'Miles Davis', 
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Miles_Davis_by_Palumbo.jpg/330px-Miles_Davis_by_Palumbo.jpg' 
+            }
+        ] 
+    },
+    {
+        id: 'luis-armstrong', 
+        title: 'Jazz', 
+        images: [
+            { 
+                title: 'Luis Armstrong', 
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Louis_Armstrong_restored.jpg/330px-Louis_Armstrong_restored.jpg' 
+            }
+        ] 
+    },
+    {
+        id: 'john-coltrane', 
+        title: 'Jazz', 
+        images: [
+            { 
+                title: 'John Coltrane', 
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/John_Coltrane_1963.jpg/330px-John_Coltrane_1963.jpg' 
+            }
+        ] 
+    }
 ];
 
 export default {
