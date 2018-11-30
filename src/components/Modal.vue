5588613a86937706ddcb03a8def96c6d310a9ca1
JeiWolfei <jvampy22@gmail.com>
2018-11-30 14:23:47 -0800

    added Modal component for AddAlbum, working

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal" @click="onClose" @keyup.esc="onClose">
+    <div class="content" @click.stop="">
+      <button class="close" @click="onClose">X</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    onClose: Function
+  },
+  created() {
+    console.log('Modal created');
+    this.documentListener = event => {
+      if(event.keyCode === 27) {
+        console.log('closing');
+        this.onClose();
+      }
+    };
+    document.addEventListener('keyup', this.documentListener);
+  }
+};
+</script>
--- src/components/albums/Albums.vue
+++ src/components/albums/Albums.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
     <h2 class="album-title">Albums</h2>
-    <!-- <button @click="showModal = true">Add New Album</button> -->
-    <NewAlbum/>
+    <button @click="showModal = true">Add New Album</button>
+    <Modal class="modal" v-if="showModal" v-bind:onClose="() => showModal = false">
+      <NewAlbum/>
+    </Modal>
     <AlbumList v-bind:albums="albums"/>
   </section>
 </template>
@@ -11,16 +13,19 @@
 import albumsApi from '../../albumsApi.js';
 import AlbumList from './AlbumList.vue';
 import NewAlbum from './NewAlbum.vue';
+import Modal from '../Modal.vue';
 
 export default {
   data() {
     return {
-      albums: albumsApi.getAlbums()
+      albums: albumsApi.getAlbums(),
+      showModal: false
     };
   },
   components: {
     AlbumList,
-    NewAlbum
+    NewAlbum,
+    Modal
   }
 };
 </script>
