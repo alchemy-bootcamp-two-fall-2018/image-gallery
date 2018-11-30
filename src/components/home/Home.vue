62576cfe3d4569e49d27143a9626f2b6c87aa732
Dyanna Tolman <dyannast@gmail.com>
2018-11-29 22:36:34 -0800

    one change

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -13,7 +13,9 @@ export default {
 <style>
 section {
     background-color:white;
-}
+    padding: 10px;
+    opacity: 0.9;
+    }
 body {
     background-image: url('https://images.unsplash.com/photo-1542315291377-a38c9fe57384?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e8c283650ed772033e7a357269dc0fc9&auto=format&fit=crop&w=800&q=60');
     background-size:cover;
