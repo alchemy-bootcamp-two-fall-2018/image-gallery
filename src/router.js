a9e2f3e132e19be45cb82699393472d15c901062
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-30 12:09:26 -0800

    trying to fix the stupid capital v in the album detail that keeps changing when it gets to github

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home';
 import Albums from './components/albums/Albums.vue';
-import AlbumDetail from './components/albums/AlbumDetail.vue';
+import AlbumDetail1 from './components/albums/AlbumDetail1.vue';
 import About from './components/about/About.vue';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/albums', component: Albums },
-    { path: '/albums/:id', component: AlbumDetail },
+    { path: '/albums/:id', component: AlbumDetail1 },
     { path: '/about', component: About },
 
 
