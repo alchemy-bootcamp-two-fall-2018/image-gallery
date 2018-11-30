1e11480db6d1a4e9b1fa76b5f2e84fbf2a08c82d
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-30 15:44:59 -0800

    beginning of listview

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,15 +1,26 @@
 <template>
     <div>
-    this is the list
+     <p>There are {{images.length}} image(s)</p>
+
+     <li 
+            v-for="(image, index) in images"
+            :key="index">
+            <h4>{{image.title}}</h4>
+            <p>{{image.url}}</p>
+        </li>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        images: Array
+    }
 };
 </script>
 
-<style>
-
+<style scoped>
+    li {
+        list-style: none;
+    }
 </style>
