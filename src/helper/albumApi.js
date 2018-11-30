f29f1419c543cec7fbf65cbe5755b2c5b1ad7a6d
Paige Gorry <paigeegorry@gmail.com>
2018-11-30 08:58:32 -0800

    added some data

--- src/helper/albumApi.js
+++ src/helper/albumApi.js
@@ -12,12 +12,26 @@ const albums = [
     {
         id: '2',
         type: 't-rex',
-        images: []
+        images: [
+            {
+                title: 'T-Rex Skeleton',
+                url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJgTYqgQX4cD1SUApQD8MCQoDBgZMX4TD0dvBlrOWgGhz-SeiF_g'
+            }
+        ]
     },
     {
         id: '3',
         type: 'long-neck',
-        images: []
+        images: [ 
+            {
+                title: 'Long Neck Dino Attack',
+                url: 'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzAyMS82MzEvb3JpZ2luYWwvYnJvbnRvbWVydXMuanBn'
+            },
+            {
+                title: 'Long Neck Taco Holder',
+                url: 'https://images-na.ssl-images-amazon.com/images/I/714ZfW4WBEL._SX425_.jpg'
+            }
+        ]
     }
 ];
 
