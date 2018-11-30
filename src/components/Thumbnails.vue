40d6e6ce0203a64c90057bda8833473a3a8db121
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-29 16:58:53 -0800

    added add functionality

--- src/components/Thumbnails.vue
+++ src/components/Thumbnails.vue
@@ -1,7 +1,12 @@
 <template>
     <ul>
-        <li>
-            Thumbnail Works
+        <li 
+            v-for="(image, index) in images"
+            :key="index">
+            <h4>{{image.title}}</h4>
+            <p>
+                <img :src="image.url">
+            </p>
         </li>
     </ul>
 </template>
--- src/services/albumsApi.js
+++ src/services/albumsApi.js
@@ -6,6 +6,10 @@ const albums = [
             {
                 title: 'this is a Dachsund',
                 url: 'https://www.rover.com/blog/wp-content/uploads/2017/06/pepper-mini-dachshund-960x540.jpg',
+            },
+            {
+                title: 'this is also a Dachsund',
+                url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxC8_mIsXGec7joCTAa8fnNkloC7bQ9lb-0IzxXHyv5qsOxJ1ocA'
             }
         ]
 
@@ -13,12 +17,20 @@ const albums = [
     {
         id: 'beagle',
         title: 'Beagles',
-        images: []
+        images: [
+            {
+                title: 'this is a Beagle',
+                url: 'https://i.pinimg.com/originals/a7/f8/70/a7f870516475a1fc3b52dad9b9ec2c4b.jpg'
+            }
+        ]
     },
 ];
 
 export default {
     getAlbums() {
         return albums;
+    },
+    getAlbum(id) {
+        return albums.find(album => album.id === id);
     }
 };
