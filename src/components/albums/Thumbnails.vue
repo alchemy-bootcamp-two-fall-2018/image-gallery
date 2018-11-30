612ca4ff282cf9fb22765330bdbfa8829ff887a2
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-29 16:20:37 -0800

    created Thumbnails component and hooked it up to AlbumDetail and we can now see PICTURES ON UI

---
+++ src/components/albums/Thumbnails.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li
+      v-for="(image, index) in images"
+      v-bind:key="index">
+      <h4>{{image.title}}</h4>
+      <p>
+        <img v-bind:src="image.url">
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    images: Array
+  }
+};
+</script>
+
+<style>
+</style>
+
