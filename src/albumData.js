00c3d8ad5a098ffcbe44178d8c50f8204af05097
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-30 13:39:10 -0800

    updated albumsApi for saving ability and moved data to albumData.js and checked that our cancel button fix works

---
+++ src/albumData.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    id: 'trees',
+    title: 'Christmas Trees',
+    images: [
+      {
+        title: 'Portland Christmas Tree',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsWrxXcUUOISDzAP18z2J_5pm7OUPDmc-LIi8MyXhv4TUKAInn'
+      },
+      {
+        title: 'NY Christmas Tree',
+        url: 'http://blog.redcarpets.com/wp-content/blogs.dir/21/files/2014/12/use-blog.jpg'
+      },
+      {
+        title: 'Dollywood Tree',
+        url: 'https://localtvwreg.files.wordpress.com/2012/12/11.jpg?quality=85&strip=all&w=750'
+      },
+      {
+        title: 'London Tree',
+        url: 'http://nebula.wsimg.com/0503318d3a008b0e6d5124b8f7c24768?AccessKeyId=AF03068AF07D0672358A&disposition=0&alloworigin=1'
+      },
+      {
+        title: 'Paris Tree',
+        url: 'http://nebula.wsimg.com/d0e8f3a6b56054a499137a4c322969bf?AccessKeyId=AF03068AF07D0672358A&disposition=0&alloworigin=1'
+      }
+    ]
+  },
+  {
+    id: 'helpers',
+    title: 'Santa Helpers',
+    images: [
+      {
+        title: 'Happy Christmas Cat',
+        url: 'https://i.pinimg.com/originals/d0/91/1b/d0911b1e862f82ddc7f53f8265435d77.jpg'
+      },
+      {
+        title: '2 Christmas Doggos',
+        url: 'https://i.pinimg.com/originals/41/e5/06/41e5066d12099d2f3cb11cfa9b77a039.jpg'
+      },
+      {
+        title: '2 Christmas Doggos',
+        url: 'https://i.pinimg.com/originals/41/e5/06/41e5066d12099d2f3cb11cfa9b77a039.jpg'
+      },
+      {
+        title: '2 Christmas Doggos',
+        url: 'https://i.pinimg.com/originals/41/e5/06/41e5066d12099d2f3cb11cfa9b77a039.jpg'
+      },
+      {
+        title: '2 Christmas Doggos',
+        url: 'https://i.pinimg.com/originals/41/e5/06/41e5066d12099d2f3cb11cfa9b77a039.jpg'
+      },
+      {
+        title: 'Cat Sweater',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQcwDL5B1xN9Amtq350OQVLSdCy9iA_RRYhvsmwF6Lk5LF_59jpQ'
+      },
+      {
+        title: 'Reindeer Lights',
+        url: 'http://images2.fanpop.com/images/photos/2700000/Christmas-Reindeer-Christnas-2008-christmas-2783191-445-301.jpg'
+      },
+      {
+        title: 'Bunny Christmas',
+        url: 'https://i.chzbgr.com/full/6917571840/hB361D059/'
+      },
+      {
+        title: '',
+        url: ''
+      }
+    ]
+  },
+  {
+    id: 'lights',
+    title: 'Christmas Lights',
+    images: [
+      {
+        title: 'Christmas Crazy',
+        url: 'https://happyholidaysblog.com/wp-content/uploads/Christmas-Lights-On-Houses-09.jpg'
+      },
+      {
+        title: 'Light Overload',
+        url: 'http://tarbell.com/newspress/wp-content/uploads/2015/12/christmas-lights-1.jpg'
+      },
+      {
+        title: 'Christmas Crazy',
+        url: 'https://happyholidaysblog.com/wp-content/uploads/Christmas-Lights-On-Houses-09.jpg'
+      },
+      {
+        title: 'Christmas Crazy',
+        url: 'https://happyholidaysblog.com/wp-content/uploads/Christmas-Lights-On-Houses-09.jpg'
+      },
+      {
+        title: 'Blue Christmas',
+        url: 'https://static1.squarespace.com/static/56feae0ab6aa60ebb6039bf3/t/5a3021fd0d9297e3b10d5663/1513103892821/24831355_10156064300274446_6063417990896438734_o.jpg'
+      }
+    ]
+  },
+  {
+    id: 'blank',
+    title: 'blank',
+    images: [
+      {
+        title: 'blank',
+        url: ''
+      },
+      {
+        title: 'blank',
+        url: ''
+      },
+      {
+        title: 'blank',
+        url: ''
+      },
+      {
+        title: 'blank',
+        url: ''
+      },
+      {
+        title: 'blank',
+        url: ''
+      }
+    ]
+  },
+];
