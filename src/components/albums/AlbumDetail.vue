62249987033a31fe3cffd5a421d5bc4d078c3dd4
Cheri Portman <cheriportman@hotmail.com>
2018-11-29 17:57:13 -0800

    wrapping up for the evening

--- src/components/albums/AlbumDetail.vue
+++ src/components/albums/AlbumDetail.vue
@@ -9,6 +9,8 @@
         <div v-if="showModal" class="modal">
             <div class="content">
                 I am the form. May the form be with you.
+                <input type="file" name="pic" accept="image/*">
+                <input type="submit">
                 <button @click="showModal = false">Close</button>
             </div>
         </div>
