98f0a11ec4a0cc20fbe7b59f7e4fe54f6a9143d1
Heather <unphurlng@gmail.com>
2018-11-30 16:00:21 -0800

    added buttons to gallery view, registering in console.

--- src/components/artists/records/GalleryView.vue
+++ src/components/artists/records/GalleryView.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
     <h3>Gallery View</h3>
-    {{albums[0]}}
+    <button @click="setIndex()">&lt;</button>
+    <button @click="setIndex()">&gt;</button>
+    <p>{{albums[0].title}}</p>
+    <p><img :src="albums[0].imgUrl"></p>
   </section>
 </template>
 
@@ -9,6 +12,11 @@
 export default {
   props: {
     albums: Array
+  },
+  methods: {
+    setIndex() {
+      console.log('clicked');
+    }
   }
 };
 </script>
