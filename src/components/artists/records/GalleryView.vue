cc94f49a9b1700905752ce4bebde7f93c6fc83b8
Shaba9 <shaba92@gmail.com>
2018-11-30 16:11:34 -0800

    gallery goes through images and title

--- src/components/artists/records/GalleryView.vue
+++ src/components/artists/records/GalleryView.vue
@@ -1,21 +1,26 @@
 <template>
   <section>
     <h3>Gallery View</h3>
-    <button @click="setIndex()">&lt;</button>
-    <button @click="setIndex()">&gt;</button>
-    <p>{{albums[0].title}}</p>
-    <p><img :src="albums[0].imgUrl"></p>
+    <button @click="setIndex(-1)">&lt;</button>
+    <button @click="setIndex(1)">&gt;</button>
+    <p>{{albums[index].title}}</p>
+    <p><img :src="albums[index].imgUrl"></p>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      index: 0
+    };
+  },
   props: {
-    albums: Array
+    albums: Array,
   },
   methods: {
-    setIndex() {
-      console.log('clicked');
+    setIndex(amount) {
+      this.index += amount;
     }
   }
 };
