dae9a2ace9717c583f39e7f670516f6ca5eaae1a
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-30 16:06:02 -0800

    ListView.vue and layout complete

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-     <p>There are {{images.length}} image(s)</p>
-
-     <li 
+     <p class="num">There are {{images.length}} image(s)</p>
+        <li 
             v-for="(image, index) in images"
             :key="index">
-            <h4>{{image.title}}</h4>
-            <p>{{image.url}}</p>
+            <img :src="image.url">
+            <div class="info">
+                <h4>{{image.title}}</h4>
+                <p>{{image.url}}</p>
+            </div>
         </li>
     </div>
 </template>
@@ -22,5 +24,19 @@ export default {
 <style scoped>
     li {
         list-style: none;
+        display: flex;
     }
+    img {
+        height: 200px;
+        width: 300px;
+        padding: 20px;
+
+    }
+    .num {
+        text-align: center;
+    }
+    .info {
+        padding-top: 60px;
+    }
+    
 </style>
