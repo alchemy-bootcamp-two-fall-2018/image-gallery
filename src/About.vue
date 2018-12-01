5b7a18f56def0ea01c37326f1e0cef4820ed4b9c
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-30 16:14:30 -0800

    start about page

---
+++ src/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        About the Developers
+    </section>
+    <div>
+        Aaron's stuff goes here!
+    </div>
+    <div>
+        Christina T. 
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
