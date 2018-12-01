1ad4377aca650aa9c7d8073e7ffd061be7e24f5f
Paige Gorry <paigeegorry@gmail.com>
2018-11-30 17:22:40 -0800

    styling done for about page

--- src/Home/About.vue
+++ src/Home/About.vue
@@ -1,5 +1,21 @@
 <template>
-    <h2>About page</h2>
+    <section>
+        <img src="https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fa5fb546c-5f68-11e8-918b-ccedfa3966cd.jpg?crop=2673%2C1504%2C22%2C252&resize=685">
+        <h2>About page</h2>
+        <div>
+            <span><h3>Dino Ipsum</h3></span>
+            <p>Blikanasaurus Leyesaurus Xuwulong Buitreraptor Pitekunsaurus Fulengia Latirhinus Oohkotokia Yamaceratops Tehuelchesaurus Ouranosaurus Zhongyuansaurus Diceratus Conchoraptor Spinosaurus Serendipaceratops Masiakasaurus Clasmodosaurus Dinodocus Suuwassea Echinodon Alectrosaurus Coelophysis Koreaceratops Massospondylus Chuandongocoelurus Loricatosaurus Jinzhousaurus Gasosaurus Eucoelophysis.</p>
+            <p>Deuterosaurus Polyodontosaurus Anoplosaurus Rugops Gasosaurus Amphisaurus Jobaria Achillesaurus Hylosaurus Qiupalong Gideonmantellia Venenosaurus Halticosaurus Angaturama Acrocanthosaurus Mononykus Clepsysaurus Sinovenator Linhenykus Polacanthus Coloradisaurus Sellacoxa Ferganocephale Xianshanosaurus Limnosaurus Appalachiosaurus Sellacoxa Wakinosaurus Ultrasaurus Archaeodontosaurus.</p>
+            <p>Neuquensaurus Betasuchus Chassternbergia Spinostropheus Abrictosaurus Itemirus Uintasaurus Caseosaurus Fulgurotherium Hippodraco Silvisaurus Lariosaurus Variraptor Quaesitosaurus Angaturama Scansoriopteryx Albalophosaurus Glyptodontopelta Prosaurolophus Shunosaurus Zizhongosaurus Syntarsus Koreaceratops Helioceratops Yanornis Mendozasaurus Patagonykus Microvenator Vagaceratops Melanorosaurus.</p>
+            <p>Texacephale Sarcolestes Libycosaurus Yangchuanosaurus Barsboldia Limaysaurus Tatankaceratops Zhuchengtyrannus Limaysaurus Compsognathus Charonosaurus Neovenator Palaeocursornis Pleuropeltus Ligabuesaurus Levnesovia Gasosaurus Rahona Bainoceratops Parksosaurus Rugops Gigantspinosaurus Tatankaceratops Olorotitan Hulsanpes Labrosaurus Gargoyleosaurus Leonerasaurus Loricosaurus Lesothosaurus.</p>
+            <p>Pellegrinisaurus Yaverlandia Achillobator Velociraptor Amurosaurus Parasaurolophus Yimenosaurus Lametasaurus Amphicoelias Talenkauen Lexovisaurus Kileskus Zatomus Rhodanosaurus Ricardoestesia Rahiolisaurus Priodontognathus Caseosaurus Styracosaurus Pneumatoarthrus Pachysaurops Utahceratops Emausaurus Acristavus Phyllodon Fulengia Sarahsaurus Blasisaurus Itemirus Batyrosaurus.</p>
+            <p>Fukuisaurus Nopcsaspondylus Yangchuanosaurus Elaphrosaurus Picrodon Yuanmousaurus Teinurosaurus Tapuiasaurus Dinodocus Taveirosaurus Sinucerasaurus Mononykus Koutalisaurus Microvenator Sulaimanisaurus Naashoibitosaurus Aristosaurus Balaur Eotyrannus Uberabatitan Hypselospinus Rileyasuchus Didanodon Chiayusaurus Beipiaosaurus Eolambia Yimenosaurus Kemkemia Gobiceratops Nanotyrannosaurus.</p>
+            <p>Australodocus Pentaceratops Eotyrannus Velocipes Albinykus Tichosteus Dracopelta Emausaurus Eosinopteryx Dyslocosaurus Shanxia Arrhinoceratops Barosaurus Tochisaurus Genusaurus Hanssuesia Aublysodon Procerosaurus Irritator Sinopliosaurus Anabisetia Supersaurus Kukufeldia Shuangmiaosaurus Jeholornis Dollodon Ceratonykus Tochisaurus Lagerpeton Yunnanosaurus.</p>
+            <p>Rhoetosaurus Bihariosaurus Macrogryphosaurus Tanystropheus Alxasaurus Mussaurus Gongbusaurus Lurdusaurus Tuojiangosaurus Maiasaura Ilokelesia Anasazisaurus Huayangosaurus Amurosaurus Muyelensaurus Barosaurus Centemodon Teinurosaurus Fruitadens Edmontosaurus Magnirostris Ornithosuchus Alamosaurus Spondylosoma Anabisetia Ornithosuchus Vitakrisaurus Trimucrodon Sterrholophus Laevisuchus.</p>
+            <p>Stokesosaurus Centemodon Sinusonasus Shunosaurus Laevisuchus Sigilmassasaurus Tethyshadros Xianshanosaurus Xixianykus Gobiceratops Caenagnathus Ahshislepelta Griphornis Abydosaurus Loricosaurus Creosaurus Zhuchengosaurus Sphenosaurus Proceratosaurus Jinzhousaurus Haplocanthosaurus Arkharavia Gongbusaurus Mongolosaurus Pachysauriscus Longosaurus Venaticosuchus Khetranisaurus Streptospondylus Homalocephale.</p>
+            <p>Sahaliyania Dandakosaurus Altirhinus Cionodon Archaeovolans Priconodon Pachysaurus Wakinosaurus Argentinosaurus Jubbulpuria Betasuchus Anchisaurus Creosaurus Unenlagia Dracorex Aralosaurus Dynamosaurus Loricatosaurus Rhabdodon Turiasaurus Pteropelyx Pinacosaurus Marisaurus Proplanicoxa Leyesaurus Kemkemia Pakisaurus Acanthopholis Auroraceratops Avimimus.</p>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -8,6 +24,21 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+span {
+    font-weight: bold;
+    text-transform: uppercase;
+}
+div {
+    width: 50vw;
+    margin: auto;
+    height: 75vh;
+    overflow: scroll;
+}
+p {
+    text-align: left;
+}
+img {
+    width: 50vw;
+}
 </style>
