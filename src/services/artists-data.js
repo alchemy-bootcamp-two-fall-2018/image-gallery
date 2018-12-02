f5f1aea00076c3fe842a7e70e9210b7516eb7e4d
Heather <unphurlng@gmail.com>
2018-12-02 15:46:13 -0800

    added artists/albums/songs

--- src/services/artists-data.js
+++ src/services/artists-data.js
@@ -1,59 +1,509 @@
 export default [
   {
-    name: 'Bart Simpson',
-    image: 'https://letsdrawkids.com/img/drawings/179-how-to-draw-bart-simpson.png',
+    name: 'Halsey',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Halsey_-_Badlands.png/220px-Halsey_-_Badlands.png',
     albums: [
       {
-        title: 'So Bad',
-        imgUrl: 'https://cdn.tinybuddha.com/wp-content/uploads/2018/03/Rainy-day-man.png'
+        title: 'Hopeless Fountain Kingdom',
+        released: 'June 2, 2017',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Halsey_-_Hopeless_Fountain_Kingdom.png/220px-Halsey_-_Hopeless_Fountain_Kingdom.png',
+        songs: [
+          'The Prologue',
+          '100 Letters', 
+          'Eyes Closed', 
+          'Alone', 
+          'Now or Never', 
+          'Sorry', 
+          'Good Mourning', 
+          'Lie', 
+          'Walls Could Talk', 
+          'Bad at Love',
+          'Strangers',
+          'Devil in Me',
+          'Hopeless'
+        ]
       },
       {
-        title: 'So Good',
-        imgUrl: 'https://www.sogood.fr/img/sogood-1453998661.jpg?1464770694'
+        title: 'Badlands',
+        released: 'August 28, 2015',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Halsey_-_Badlands.png/220px-Halsey_-_Badlands.png',
+        songs: [
+          'Castle',
+          'Hold Me Down',
+          'New Americana',
+          'Drive',
+          'Roman Holiday',
+          'Colors',
+          'Coming Down',
+          'Haunting',
+          'Control',
+          'Young God',
+          'Ghost'
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Lily Allen',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Lily_Allen_-_No_Shame.png/220px-Lily_Allen_-_No_Shame.png',
+    albums: [
+      {
+        title: 'No Shame',
+        released: 'June 8, 2018',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Lily_Allen_-_No_Shame.png/220px-Lily_Allen_-_No_Shame.png',
+        songs: [
+          'Come on Then',
+          'Trigger Bang',
+          'What You Waiting For?',
+          'Your Choice',
+          'Lost My Mind',
+          'Higher',
+          'Family Man',
+          'Apples',
+          'Three',
+          'Everything to Feel Something',
+          'Waste',
+          'My One',
+          'Pushing Up Daisies',
+          'Cake'
+        ]
       },
       {
-        title: 'So Bad It\'s Good',
-        imgUrl: 'http://travel.spotcoolstuff.com/wp-content/uploads/2008/04/moba-m.jpg'
+        title: 'Sheezus',
+        released: 'May 2, 2014',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Lily_Allen_-_Sheezus.png/220px-Lily_Allen_-_Sheezus.png',
+        songs: [
+          'Sheezus',
+          'L8 CMMR',
+          'Air Balloon',
+          'Our Time',
+          'Insincerely Yours',
+          'Take My Place',
+          'As Long As I Got You',
+          'Close Your Eyes',
+          'URL Badman',
+          'Silver Spoon',
+          'Life for Me',
+          'Hard Out Here',
+          'Interlude',
+          'Somewhere Only We Know'
+        ]
+      },
+      {
+        title: 'It\'s Not Me, It\'s You',
+        released: 'February 4, 2009',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Lily_Allen_-_It%27s_Not_Me%2C_It%27s_You.png/220px-Lily_Allen_-_It%27s_Not_Me%2C_It%27s_You.png',
+        songs: [
+          'Everyone\'s at it',
+          'the Fear',
+          'Not Fair',
+          '22',
+          'I Could Say',
+          'Back to the Start',
+          'Never Gonna Happen',
+          'Fuck You',
+          'Who\'d Have Known',
+          'Chinese',
+          'Him',
+          'He Wasn\'t There'
+        ]
+      },
+      {
+        title: 'Alright, Still',
+        released: 'July 13, 2006',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Lily_Allen_-_Alright%2C_Still.png/220px-Lily_Allen_-_Alright%2C_Still.png',
+        songs: [
+          'Smile',
+          'Knock \'Em Out',
+          'LDN',
+          'Everything\'s Just Wonderful',
+          'Not Big',
+          'Friday Night',
+          'Shame for You',
+          'Littlest Things',
+          'Take What You Take',
+          'Friend of Mine',
+          'Alfie'
+        ]
       }
     ]
 
   },
   {
-    name: 'Homer Simpson',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png',
+    name: 'Tinariwen',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Tassili_album.jpg/220px-Tassili_album.jpg',
     albums: [
       {
-        title: 'Too Bad',
-        imgUrl: 'https://cdn.tinybuddha.com/wp-content/uploads/2018/03/Rainy-day-man.png'
+        title: 'Elwan',
+        released: 'February 10, 2017',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Tinariwen_Elwan.jpg/220px-Tinariwen_Elwan.jpg',
+        songs: [
+          'Tiwàyyen',
+          'Sastanàqqàm',
+          'Nizzagh Ijbal',
+          'Hayati',
+          'Ittus',
+          'Ténéré Tàqqàl',
+          'Imidiwàn n-àkall-in',
+          'Talyat',
+          'Assàwt',
+          'Arhegh ad annàgh',
+          'Nànnuflày',
+          'Intro Flute Fog Edaghàn',
+          'Fog Edaghàn'
+        ]
       },
       {
-        title: 'Too Good',
-        imgUrl: 'https://www.sogood.fr/img/sogood-1453998661.jpg?1464770694'
+        title: 'Emmaar',
+        released: 'February 10, 2014',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Tinariwen_Emmaar.jpg/220px-Tinariwen_Emmaar.jpg',
+        songs: [
+          'Toumast Tincha',
+          'Chaghaybou',
+          'Arhegh Danagh',
+          'Timadrit In Sahara',
+          'Imidiwan Ahi Sigdim',
+          'Tahalamot',
+          'Sendad Eghlalan',
+          'Imidiwanin Ahi Tifhamam',
+          'Koud Edhaz Emin',
+          'Emajer',
+          'Aghregh Medin',
+          'Adounia Ti Chidjret',
+          'Islegh Taghram Tifhamam',
+          'Tin Ihlan'
+        ]
       },
       {
-        title: 'Too Bad It\'s Good',
-        imgUrl: 'http://travel.spotcoolstuff.com/wp-content/uploads/2008/04/moba-m.jpg'
+        title: 'Tassili',
+        released: 'August 29, 2011',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Tassili_album.jpg/220px-Tassili_album.jpg',
+        songs: [
+          'Imidiwan Ma Tenam',
+          'Asuf D Alwa',
+          'Tenere Taqhim Tossam',
+          'Ya Messinagh',
+          'Walla Illa',
+          'Tameyawt',
+          'Imidiwan Win Sahara',
+          'Tamiditin Tan Ufrawan',
+          'Tiliaden Osamant',
+          'Djeredjere',
+          'Iswegh Attay',
+          'Takest Tamidaret'
+        ]
+      },
+      {
+        title: 'Imidiwan: Companions',
+        released: 'June 29, 2009',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Tinariwen_Imidwan.jpeg/220px-Tinariwen_Imidwan.jpeg',
+        songs: [
+          'Imidiwan Afrik Tendam',
+          'Lulla',
+          'Tenhert',
+          'Enseqi Ehad Didagh',
+          'Tahult In',
+          'Tamudjeras Assis',
+          'Intitlayaghen',
+          'Imazaghen N Adagh',
+          'Tenalle Chegret',
+          'Kel Tamashek',
+          'Assuf Ag Assuf',
+          'Chabiba',
+          'Ere Tasfata Adounia',
+          'Desert Wind'
+        ]
+      },
+      {
+        title: 'Aman Iman: Water is Life',
+        released: 'March 13, 2007',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/63/AmanIman.jpg/220px-AmanIman.jpg',
+        songs: [
+          'Cler Achel',
+          'Mano Dayak',
+          'Matadjem Yinmixan',
+          'Ahimana',
+          'Soixante Trois',
+          'Toumast',
+          'Imidiwan Winakalin',
+          'Awa Didjen',
+          'Ikyadarh Dim',
+          'Tamatant Tilay',
+          'Assouf',
+          'Izarharh Tenere'
+        ]
       }
     ]
-
   },
   {
-    name: 'Lisa Simpson',
-    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Lisa_Simpson.png/220px-Lisa_Simpson.png',
+    name: 'Aretha Franklin',
+    image: 'https://upload.wikimedia.org/wikipedia/en/1/1c/Aretha.jpg',
     albums: [
       {
-        title: 'Very Bad',
-        imgUrl: 'https://cdn.tinybuddha.com/wp-content/uploads/2018/03/Rainy-day-man.png'
+        title: 'Aretha Franklin Sings the Great Diva Classics',
+        released: 'October 17, 2014',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/Aretha_Franklin_Sings_the_Great_Diva_Classics.jpg/220px-Aretha_Franklin_Sings_the_Great_Diva_Classics.jpg',
+        songs: [
+          'At Last',
+          'Rolling in the Deep',
+          'Midnight Train to Georgia',
+          'I will Survive',
+          'People',
+          'No One',
+          'I\'m Every Woman',
+          'Teach Me Tonight',
+          'You Keep Me Hangin\' On',
+          'Nothing Compares 2 U'
+        ]
       },
       {
-        title: 'Very Good',
-        imgUrl: 'https://www.sogood.fr/img/sogood-1453998661.jpg?1464770694'
+        title: 'Who\s Zommin\' Who?',
+        released: 'July 9, 1985',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Who%27s_Zoomin_Who_album.jpg/220px-Who%27s_Zoomin_Who_album.jpg',
+        songs: [
+          
+          'Freeway Of Love',
+          'Another Night',
+          'Sweet Bitter Love',
+          'Who\'s Zoomin\' Who',
+          'Sisters Are Doin\' It For Themselves',
+          'Until You Say You Love Me',
+          'Ain\'t Nobody Ever Loved You',
+          'Push',
+          'Integrity'
+        ]
       },
       {
-        title: 'Very Bad It\'s Good',
-        imgUrl: 'http://travel.spotcoolstuff.com/wp-content/uploads/2008/04/moba-m.jpg'
+        title: 'Amazing Grace',
+        released: 'June 1, 1972',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/ArethaAmazingGrace.JPG/220px-ArethaAmazingGrace.JPG',
+        songs: [
+          'Mary, Don\'t You Weep',
+          'Precious Lord, Take My Hand / You\'ve Got A Friend',
+          'Old Landmark',
+          'Give Yourself To Jesus',
+          'How I Got Over',
+          'What A Friend We Have In Jesus',
+          'Amazing Grace',
+          'Precious Memories',
+          'Climbing Higher Mountains',
+          'Remarks by Reverend C.L. Franklin',
+          'Tod Will Take Care Of You',
+          'Wholy Holy',
+          'You\'ll Never Walk Alone',
+          'Never Grow Old'
+        ]
+      },
+      {
+        title: 'I Never Loved A Man The Way I Love You',
+        released: 'March 10, 1967',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Aretha_Franklin_%E2%80%93_I_Never_Loved_a_Man_the_Way_I_Love_You.jpg/220px-Aretha_Franklin_%E2%80%93_I_Never_Loved_a_Man_the_Way_I_Love_You.jpg',
+        songs: [
+          'Respect',
+          'Drown In My Own Tears',
+          'I Never Loved A Man (The Way I Love You)',
+          'Soul Serenade',
+          'Don\'t Let Me Lose This Dream',
+          'Baby Baby Baby',
+          'Dr. Feelgood',
+          'Good Times',
+          'Do Right Woman, Do Right Man',
+          'Save Me',
+          'A Change Is Gonna Come'
+        ]
+      },
+      {
+        title: 'Laughing on the Outside',
+        released: 'August 12, 1963',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Laughing_On_The_Outside.jpg/220px-Laughing_On_The_Outside.jpg',
+        songs: [
+          'Skylark',
+          'For All We Know',
+          'Make Someone Happy',
+          'I wonder',
+          'Solitude',
+          'Laughing On The Outside',
+          'Say It Isn\'t So',
+          'Until The Real Thing Comes Along',
+          'If Ever I Would Leave You',
+          'Where Are You?',
+          'Mr. Ugly',
+          'I Wanna Be Around',
+          'Ol\' Man River,'
+        ]
       }
     ]
-
+  },
+  {
+    name: '',
+    image: '',
+    albums: [
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      },
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      },
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      },
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      },
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      },
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      }
+    ]
+  },
+  {
+    name: '',
+    image: '',
+    albums: [
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      },
+      {
+        title: '',
+        released: '',
+        imgUrl: '',
+        songs: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      }
+    ]
   }
 ];
