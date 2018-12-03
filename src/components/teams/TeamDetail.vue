15de50c4f7996c034d3c70ff2b0245683c5ab31c
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-12-02 20:24:31 -0800

    nav bar links centered

--- src/components/teams/TeamDetail.vue
+++ src/components/teams/TeamDetail.vue
@@ -38,16 +38,6 @@ export default {
 
 <style>
 
-.modal {
-    position: fixed;
-    top: 0; left: 0;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, .5);
-}
 .content {
     background: white;
     padding: 40px;
